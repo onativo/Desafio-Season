22569c17adb54cd23f9f52ba04cb08052538000d
onativo <onativo@outlook.com>
2022-06-30 19:38:07 -0300

    Updates

---
+++ precache-manifest.16bb87724b46984640350e515ade4e2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb6e226509c3cb625489",
+    "url": "/Desafio-Season/static/css/main.86f4cfb5.chunk.css"
+  },
+  {
+    "revision": "fb6e226509c3cb625489",
+    "url": "/Desafio-Season/static/js/main.fb6e2265.chunk.js"
+  },
+  {
+    "revision": "af08dc77e4e76da41e54",
+    "url": "/Desafio-Season/static/js/1.af08dc77.chunk.js"
+  },
+  {
+    "revision": "f8ad30a0fcda66f51061",
+    "url": "/Desafio-Season/static/js/2.f8ad30a0.chunk.js"
+  },
+  {
+    "revision": "1c068dd2cf9406b96465",
+    "url": "/Desafio-Season/static/js/runtime~main.1c068dd2.js"
+  },
+  {
+    "revision": "fbef03d36c5ce6e416a539e25d983b7d",
+    "url": "/Desafio-Season/static/media/notfound.fbef03d3.png"
+  },
+  {
+    "revision": "1bfbbd467f00766c8dbb717cc7f444a1",
+    "url": "/Desafio-Season/index.html"
+  }
+];
